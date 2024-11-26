7aebf51c599f6e0b4fb5a490b36061f3373ddc31
Paul <p.salaun99@gmail.com>
2024-11-26 18:03:08 +0100

    🛣️ add temporary Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./pages/header/header.component";
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    HeaderComponent
   ]
 })
 export class AppComponent {
