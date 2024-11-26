9fd4e2a5f44b085d023bbb707c6a61aa724f3be5
paul.salaun <p.salaun99@gmail.com>
2024-11-26 09:55:43 +0100

    Start composing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {ConditionsComponent} from "./pages/conditions/conditions.component";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'conditions', component: ConditionsComponent },
+  // { path: 'delete-account', component: DeleteAccountComponent },
+  { path: '**', component: HomeComponent }
+];
