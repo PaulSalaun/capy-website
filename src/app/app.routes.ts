8a0905f28de4cbb03276421c458f90f58c7315ef
Paul <p.salaun99@gmail.com>
2024-11-26 16:08:41 +0100

    🚨Politique de confidentialité

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {ConditionsComponent} from "./pages/conditions/conditions.component";
-import {AppComponent} from "./app.component";
 import {HomeComponent} from "./pages/home/home.component";
 
 export const routes: Routes = [
-  { path: 'conditions', component: ConditionsComponent },
+  { path: 'privacy', component: ConditionsComponent },
   // { path: 'delete-account', component: DeleteAccountComponent },
   { path: '**', component: HomeComponent }
 ];
