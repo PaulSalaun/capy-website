c2aed41e46b7660cc93534917b6961c97ba8bfdc
Paul <p.salaun99@gmail.com>
2024-11-26 15:12:29 +0100

    ✒️ Home page

--- src/app/pages/conditions/conditions.component.ts
+++ src/app/pages/conditions/conditions.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-conditions',
   templateUrl: './conditions.component.html',
-  styleUrl: './conditions.component.scss'
+  styleUrl: './conditions.component.scss',
+  standalone: true,
+  imports: []
 })
 export class ConditionsComponent {
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {PhoneComponent} from "../components/phone/phone.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  standalone: true,
+  imports: [
+    PhoneComponent
+  ]
 })
 export class HomeComponent {
 
