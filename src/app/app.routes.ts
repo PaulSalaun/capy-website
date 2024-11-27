1fcb95b350b58f276313e6620c2c0a1f1a0580f5
Paul <p.salaun99@gmail.com>
2024-11-27 14:46:34 +0100

    🦹 Suppress component 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import {Routes} from '@angular/router';
 import {ConditionsComponent} from "./pages/conditions/conditions.component";
 import {HomeComponent} from "./pages/home/home.component";
+import {DeleteComponent} from "./pages/delete/delete.component";
 
 export const routes: Routes = [
   { path: 'privacy', component: ConditionsComponent },
   { path: 'capy-website/privacy', component: ConditionsComponent },
+  { path: 'delete', component: DeleteComponent },
+  { path: 'capy-website/delete', component: DeleteComponent },
   // { path: 'delete', component:  },
   { path: '**', component: HomeComponent }
 ];
