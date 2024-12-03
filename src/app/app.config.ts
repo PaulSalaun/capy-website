e53f66882feeb91d6769ec1e952591b196799a71
Paul <p.salaun99@gmail.com>
2024-12-03 09:54:59 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,15 +6,7 @@ import {provideAnimationsAsync} from '@angular/platform-browser/animations/async
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from "@angular/fire/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDA2o8gv3pw2gV1fqWNB_Nb2MbxegAAeMs",
-  authDomain: "kapy-38ecd.firebaseapp.com",
-  projectId: "kapy-38ecd",
-
-  storageBucket: 'kapy-38ecd.appspot.com',
-  appId: '1:481737200106:android:ef715bbe2fa8126195a293'
-};
+import {firebaseConfig} from "./environment";
 
 export const appConfig: ApplicationConfig = {
   providers: [
